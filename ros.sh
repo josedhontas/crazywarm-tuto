9e5951f8730fee62d4918ed8338474272843c56a
José Dhonatas Alves Sales <75862011+josedhontas@users.noreply.github.com>
2024-03-27 13:53:39 -0300

    Update ros.sh

--- ros.sh
+++ ros.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+c ../
+
 locale  # check for UTF-8
 
 sudo apt update && sudo apt install locales
@@ -9,10 +11,10 @@ export LANG=en_US.UTF-8
 
 locale  
 
-sudo apt install -y software-properties-common  # Adicionado "-y" aqui
+sudo apt install -y software-properties-common  
 sudo add-apt-repository universe
 
-sudo apt update && sudo apt install -y curl  # Adicionado "-y" aqui
+sudo apt update && sudo apt install -y curl  
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
